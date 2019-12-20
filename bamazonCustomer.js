e4fc876aee2ba0fb0dc2a6e5e9703aee86015229
Martin Nguyen <ngumartin@gmail.com>
2019-12-20 13:28:36 -0800

    rewriting function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,20 +11,20 @@ let connection = mysql.createConnection({
 })
 
 //MAIN CHECK AND BUY FUNCTION WHICH DISPLAYS ALL ITEMS FROM MY SQL AND THEN ADDS FUNCTIONALITY TO BUY AN ITEM WITH QUANTITIY CHOICES. 
-let checkAndBuy2 = function () {
+let checkAndBuy = function () {
     connection.query('SELECT * FROM products', function (err, res) {
-        //CREATES A NEW TABLE IN THE COOL CLI VIEW 
+        //CREATES A NEW TABLE  
         let table = new Table({
             head: ['ID', 'Product Name', 'Department', 'Price', 'Stock Quantity']
         });
 
         //DISPLAYS ALL ITEMS FOR SALE 
-        console.log("HERE ARE ALL THE ITEMS AVAILABLE FOR SALE: ");
-        console.log("===========================================");
+        console.log("================================================================\n");
+        console.log("PLEASE TAKE A LOOK AT OUR ITEMS, CHRISTMAS SALE GOING ON NOW !!!");
+        console.log("================================================================");
         for (let i = 0; i < res.length; i++) {
             table.push([res[i].id, res[i].ProductName, res[i].DepartmentName, res[i].Price, res[i].StockQuantity]);
         }
-        console.log("-----------------------------------------------");
         //LOGS THE TABLE WITH ITEMS IN FOR PURCHASE. 
         console.log(table.toString());
         inquirer.prompt([{
@@ -53,24 +53,45 @@ let checkAndBuy2 = function () {
             let chosenId = answer.itemId - 1
             let chosenProduct = res[chosenId]
             let chosenQuantity = answer.Quantity
-            if (chosenQuantity < res[chosenId].StockQuantity) {
+            if (chosenQuantity <= res[chosenId].StockQuantity) {
                 console.log("Your total for " + "(" + answer.Quantity + ")" + " - " + res[chosenId].ProductName + " is: " + res[chosenId].Price * chosenQuantity);
                 connection.query("UPDATE products SET ? WHERE ?", [{
                     StockQuantity: res[chosenId].StockQuantity - chosenQuantity
-                }, {
-                    id: res[chosenId].id
-                }], function (err, res) {
-                    //console.log(err);
-                    checkAndBuy2();
-                });
+                }, { id: res[chosenId].id }], function (err, res) {
+                    if (err) throw err;
 
-            } else {
-                console.log("Sorry, insufficient Quanity at this time. All we have is " + res[chosenId].StockQuantity + " in our Inventory.");
-                checkAndBuy2();
+                    else {
+                        if (chosenProduct.chosenQuantity > chosenId.StockQuantity) {
+                            console.log("==========================================================================================");
+                            console.log("Sorry, insufficient quanity in our inventory at this time, please choose a smaller amount.");
+                            console.log("========================================================================================\n");
+
+                        };
+
+                        inquirer
+                            .prompt({
+                                name: 'next',
+                                type: "input",
+                                message: 'Would you like to place another order (yes/no)?\n',
+                            })
+                            .then(function (answer) {
+                                if (answer.next === "yes") {
+                                    checkAndBuy();
+                                } else {
+                                    connection.end()
+                                    console.log("===============================================");
+                                    console.log("Thank you for shopping with us! Come back soon!")
+                                    console.log("===============================================\n");
+
+                                }
+
+                            });
+                    }
+                })
             }
-        })
+        });
     })
+
 };
 
-
-checkAndBuy2();
+checkAndBuy();
